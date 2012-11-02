e0bfb7c83868ffb373c67f85c12ffdef0645c7a5
Ashwini Dhekane <ashwini@ashwinidhekane.com>
2012-11-02 19:09:30 +0530

    Adding Schemes to project

---
+++ schemes/Default.php
@@ -0,0 +1,20 @@
+<?php
+
+class DefaultScheme implements SchemeIf {
+
+    public static function getErrorSheme() {
+
+    }
+    public static function getWarningScheme() {
+
+    }
+    public static function getDefaultSheme() {
+
+    }
+    public static function getSuccessScheme() {
+        
+    }
+
+}
+
+?>
